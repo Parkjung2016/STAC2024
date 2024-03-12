30714cf5bac5ab516bb1b1a063c4e468712c6ba3
ljs1206 <123707192+ljs1206@users.noreply.github.com>
2024-03-13 00:49:38 +0900

    FireBlot완성

--- Assets/99.Work/Lee/Script/Projectile.cs
+++ Assets/99.Work/Lee/Script/Projectile.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField]
-    private float _projectileSpeed;
+    private float _projectileSpeed = 3;
     [SerializeField]
-    private float _intersection;
+    private float _intersection = 30;
+    [SerializeField]
+    private GameObject _explosionPF;
 
     private Transform _playerTrm;
     private Camera _mainCam;
     private float _projectileDir;
     private Vector3 originPos;
+    private SpriteRenderer _sp;
 
     private void Awake()
     {
+        _sp = GetComponent<SpriteRenderer>();
         _playerTrm = GameObject.FindWithTag("Test").transform;
         _mainCam = Camera.main;
         originPos = transform.position;
@@ -39,6 +43,9 @@ public class Projectile : MonoBehaviour
             _projectileDir = 1;
         }
 
+        Vector3 scale = transform.localScale;
+        transform.localScale = new Vector3(scale.x * _projectileDir * -1, scale.y, scale.z);
+
         //print($"{mousePos.normalized}, {_playerTrm.up} \n {Vector3.Dot(new Vector3(_playerTrm.up.x, 0), new Vector3(mousePos.x, 0).normalized)}");
     }
 
@@ -61,6 +68,8 @@ public class Projectile : MonoBehaviour
         {
             // hit event
             print($"hit{collision.gameObject}");
+            Instantiate(_explosionPF, collision.transform.position, Quaternion.identity);
+            Destroy(gameObject);
         }
     }
 }
