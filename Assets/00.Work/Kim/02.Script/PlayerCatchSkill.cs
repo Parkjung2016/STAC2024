66de077bc7f62a701671a33f4bfc0e633ab1f895
ujn007 <hyunjun9531@gmail.com>
2024-03-15 00:25:48 +0900

    FinishHandSkill

--- Assets/00.Work/Kim/02.Script/PlayerCatchSkill.cs
+++ Assets/00.Work/Kim/02.Script/PlayerCatchSkill.cs
@@ -1,14 +1,6 @@
 using DG.Tweening;
 using System.Threading.Tasks;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using Sequence = DG.Tweening.Sequence;
-
-public enum HandSkill
-{
-    Original,
-    Conbine
-}
 
 public class PlayerCatchSkill : MonoBehaviour
 {
@@ -16,35 +8,38 @@ public class PlayerCatchSkill : MonoBehaviour
     //끌고 오는 속도임. 값을 높일 수록 더 빠르게 옴 ㅇㅇ
     [SerializeField] private float comeSpeed;
     [SerializeField] private int maxRange;
-    [SerializeField] private HandSkill handskill;
     [SerializeField][Range(1, 14)] float range;
     [SerializeField] LayerMask whatIsEnemy;
 
-    [Header("FusionSkill")]
-    [SerializeField] private GameObject explosion;
-
+    private DamageCaster damageCaster;
     private SpriteRenderer sp;
     private TrailRenderer trailRenderer;
     private Animator animator;
+    private Vector2 defaultKnockbackPower;
     Enemy enemy;
-    Transform playerTrm;
+    Transform playerVisual;
+    Player player;
 
     private float startXPos;
-    private bool isHitEnemy = true;
+    public bool isCatchEnemy { get; private set; }
 
     private void Awake()
     {
         sp = GetComponent<SpriteRenderer>();
         trailRenderer = GetComponentInChildren<TrailRenderer>();
         animator = GetComponent<Animator>();
-        playerTrm = GameManager.Instance.Player.transform.Find("Visual");
+        player = GameManager.Instance.Player;
+        playerVisual = player.transform.Find("Visual");
+        damageCaster = player.transform.GetComponentInChildren<DamageCaster>();
+
+        defaultKnockbackPower = damageCaster.knockbackPower;
     }
 
     private void Start()
     {
         startXPos = transform.position.x;
 
-        transform.DOMoveX(transform.position.x + maxRange * Mathf.Sign(playerTrm.rotation.y), arrivalTime).SetEase(Ease.Linear);
+        transform.DOMoveX(transform.position.x + maxRange * Mathf.Sign(playerVisual.rotation.y), arrivalTime).SetEase(Ease.Linear);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -62,7 +57,7 @@ public class PlayerCatchSkill : MonoBehaviour
             trailRenderer.material.DOFade(0, 0.5f).OnComplete(() => Destroy(gameObject));
         }
 
-        if(enemy != null)
+        if (enemy != null)
             enemy.StopImmediately(false);
     }
 
@@ -70,38 +65,44 @@ public class PlayerCatchSkill : MonoBehaviour
     {
         if (col.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            enemy = col.GetComponent<Enemy>();  
+            enemy = col.GetComponent<Enemy>();
 
             transform.DOKill();
             animator.SetTrigger("isCatch");
             enemy.GetComponent<Rigidbody2D>().gravityScale = 0;
+            enemy.AnimatorCompo.speed = 0;
 
-            if (handskill == HandSkill.Conbine)
-            {
-                Instantiate(explosion, transform.position, Quaternion.identity);
-            }
+            damageCaster.knockbackPower = Vector2.zero;
+
+            isCatchEnemy = true;
 
             await Task.Delay(500);
 
             PullEnemy();
-
         }
     }
 
     private void PullEnemy()
     {
-        float ChkplayerAndskill = transform.position.x - playerTrm.position.x > 0 ? 0 : 180;
+        float ChkplayerAndskill = transform.position.x - player.transform.position.x > 0 ? 180 : 0;
+        print(ChkplayerAndskill);
         transform.rotation = Quaternion.Euler(0, ChkplayerAndskill, 0);
 
         enemy.transform.parent = transform;
+        enemy.transform.localPosition = Vector2.zero;
 
-        float dis = Vector2.Distance(playerTrm.position, enemy.transform.position);
-        transform.DOMove(new Vector2(playerTrm.position.x + 1.5f, playerTrm.position.y), dis / comeSpeed)
+        float dis = Vector2.Distance(player.transform.position, enemy.transform.position);
+        transform.DOMove(new Vector2(player.transform.position.x + 1.5f, player.transform.position.y), dis / comeSpeed)
             .OnComplete(async () =>
             {
-                enemy.GetComponent<Rigidbody2D>().gravityScale = 4;
                 await Task.Delay(1000);
                 enemy.transform.parent = null;
+                enemy.AnimatorCompo.speed = 1f;
+                damageCaster.knockbackPower = defaultKnockbackPower;
+                enemy.GetComponent<Rigidbody2D>().gravityScale = 4;
+
+                isCatchEnemy = false;
+
                 sp.DOFade(0, 0.5f).OnComplete(() => Destroy(transform.gameObject));
             });
     }
